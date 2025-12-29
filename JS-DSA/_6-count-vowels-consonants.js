9725aa34b8b9b19240c64845a2af63d58a1e1d5b
Makeshkumar103 <makeshlg6@gmail.com>
2025-12-29 18:16:42 +0530

    Error corrected in exercise 6 and all file name is renamed

--- JS-DSA/_6-count-vowels-consonants.js
+++ JS-DSA/_6-count-vowels-consonants.js
@@ -1,6 +1,6 @@
 function count(string) {
-    let consonants = string.match(/[bcdfghjklmnpqrstvxyz]ig);
-    let vowels = string.match(/[aeiou]ig);
+    let consonants = string.match(/[bcdfghjklmnpqrstvxyz]/ig);
+    let vowels = string.match(/ [aeiou] /ig);
     return `Vowels:${vowels.length}, Consonants:${consonants.length}`;
 }
 
